62ba7a9c50906fce5f0a7655fb559b2dcc56cdd7
Team-CodeClan <codeclan.2226@gmail.com>
2024-07-15 00:13:13 +0530

    Update usage.py

--- CODES/usage.py
+++ CODES/usage.py
@@ -1,9 +1,9 @@
 from vehicle_simulation import VehicleSimulation
 
-csv_path = r'D:\intel\datasets\sim_fina\26_06_simrun_pt4.csv'
-road_shapefile = r'D:\intel\mergedroad_part\mergedroad.shx'
-mainroad_shapefile = r'D:\intel\mainroad_part\mainroads_part1.shx'
-output_csv_path = r'26_06_simout_pt7.csv'
+csv_path = r'Datasets/GPS_input_simulation.zip'#too big so path of zip given change while running with actual path
+road_shapefile = r'Datasets/Roads-Shapefiles/Combined/mergedroad.shx'
+mainroad_shapefile = r'Datasets/Roads-Shapefiles/Highways/mainroads_part1.shx'
+output_csv_path = r'Datasets/simulated_output.csv'
 
 simulation = VehicleSimulation(csv_path, road_shapefile, mainroad_shapefile, output_csv_path)
 simulation.load_data()
